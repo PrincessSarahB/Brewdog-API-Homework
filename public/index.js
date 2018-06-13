3c39df88da9176cdf0a9085ec533978bc3073d29
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-13 19:25:30 +0100

    lists beers and has image

--- public/index.js
+++ public/index.js
@@ -14,9 +14,22 @@ const makeRequest = function(url, callback){
 const requestComplete = function(){
   if(this.status !== 200) return;
   const beers = JSON.parse(this.response);
+  populateBeerList(beers);
+}
+
+const populateBeerList = function(beers){
+  const ul = document.querySelector('#beer-list');
+  beers.forEach(function(beer){
+    const li = document.createElement('li');
+    li.textContent = beer.name + ", " + beer.tagline;
+    const image = document.createElement('img');
+    image.src = beer.image_url;
+    image.height = 70;
+    ul.appendChild(li);
+    li.appendChild(image);
+  });
 }
 
 
 
-
- window.addEventListener('load', app);
+window.addEventListener('load', app);

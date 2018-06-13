65c3f9e7eed83101f45f9b48f5fdf163e52495c4
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-13 19:33:08 +0100

    created dropdown list of beers

--- public/index.js
+++ public/index.js
@@ -15,19 +15,30 @@ const requestComplete = function(){
   if(this.status !== 200) return;
   const beers = JSON.parse(this.response);
   populateBeerList(beers);
+  dropdownBeerList(beers);
 }
 
 const populateBeerList = function(beers){
   const ul = document.querySelector('#beer-list');
   beers.forEach(function(beer){
     const li = document.createElement('li');
-    li.textContent = beer.name + ", " + beer.tagline;
+    li.textContent = beer.name + " "
     const image = document.createElement('img');
     image.src = beer.image_url;
     image.height = 70;
     ul.appendChild(li);
     li.appendChild(image);
   });
+}
+
+const dropdownBeerList = function(beers){
+    const dropdown = document.querySelector('#beers-dropdown');
+    beers.forEach(function(beer){
+      const option = document.createElement('option');
+      option.value = beers.indexOf(beer);
+      option.textContent = beer.name;
+      dropdown.appendChild(option);
+      });
 }
 
 
